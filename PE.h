06761293ecb3874f3a3ee061bbe16e54eaec84ca
m0rfl1ng <74087498+m0rfl1ng@users.noreply.github.com>
2020-11-10 12:05:49 +0330

    Update PE.h

--- PE.h
+++ PE.h
@@ -18,6 +18,8 @@ private:
 	LPVOID Lp_Of_FileBase = NULL;
 	PIMAGE_DOS_HEADER DosHeader_Of_File;
 	PIMAGE_NT_HEADERS PeHeader_Of_file;
+	IMAGE_FILE_HEADER Image_Header_Of_File; //the next 20 bytes after pe signature aka  50h, 45h, 00h, 00h
+	PIMAGE_SECTION_HEADER Sections_Of_File;
 
 public:
 	void LoadFile(std::string);
@@ -26,7 +28,14 @@ public:
 	void SetDosHeader();
 	bool IsExe();
 	void SetPeHeader();
-
+	DWORD PeSingnature();
+	void SetImageHeader();
+	WORD Machine();
+	WORD MagicValue();
+	WORD NumberOfSections();
+	DWORD SizeOfImage();
+	DWORD SizeOfOptionalHeader();
+	void FetchSectionsOfFile();
 };
 
 #endif
