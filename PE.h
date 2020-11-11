81f9b6db4993a49b34d2d5ca53ef43794d496248
m0rfl1ng <74087498+m0rfl1ng@users.noreply.github.com>
2020-11-11 22:51:46 +0330

    Update PE.h

--- PE.h
+++ PE.h
@@ -17,10 +17,11 @@ private:
 	HANDLE Hanlde_Of_FileMapping = NULL;
 	LPVOID Lp_Of_FileBase = NULL;
 	PIMAGE_DOS_HEADER DosHeader_Of_File;
-	PIMAGE_NT_HEADERS PeHeader_Of_file;
+	//PIMAGE_NT_HEADERS PeHeader_Of_file;
+	PIMAGE_NT_HEADERS64 PeHeader_Of_file;
+	IMAGE_OPTIONAL_HEADER64 OptionalHeader;
 	IMAGE_FILE_HEADER Image_Header_Of_File; //the next 20 bytes after pe signature aka  50h, 45h, 00h, 00h
 	PIMAGE_SECTION_HEADER Sections_Of_File;
-
 public:
 	void LoadFile(std::string);
 	void MapFile();
@@ -35,6 +36,7 @@ public:
 	WORD NumberOfSections();
 	DWORD SizeOfImage();
 	DWORD SizeOfOptionalHeader();
+	ULONGLONG ImageBase();
 	void FetchSectionsOfFile();
 };
 
